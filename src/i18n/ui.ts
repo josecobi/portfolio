2a7e89819df14ea2cfa25414dabbb9c8a128b377
josecobi <58313777+josecobi@users.noreply.github.com>
2024-08-15 15:55:20 -0400

    added languages,  types, and translations to ui

---
+++ src/i18n/ui.ts
@@ -0,0 +1,34 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+    'nav.experience': 'Experience',
+    'nav.top': 'Top',
+    'nav.contact': 'Contact',
+    'nav.language': 'Language',
+    
+
+    
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Sobre mí',
+    'nav.projects': 'Proyectos',
+    'nav.experience': 'Experiencia',
+    'nav.top': 'Inicio',
+    'nav.contact': 'Contacto',
+    'nav.language': 'Idioma',
+
+
+  },
+} as const;
+
+  // export const showDefaultLang = false;
