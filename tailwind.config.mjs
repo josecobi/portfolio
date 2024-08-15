2e2a67fe718c743f1eb0e2561ae4359f63a31b92
josecobi <58313777+josecobi@users.noreply.github.com>
2024-08-15 15:49:57 -0400

    added boxshadow and darkMode class config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+			'inset': 'inset 0 0 0.8px 2px rgba(255, 255, 255, 1)',
+		  }
+		},
+		
 	},
-	plugins: [],
+	darkMode: 'class',
+	
 }
